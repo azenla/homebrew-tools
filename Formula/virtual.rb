b7cdfe62cee0320e96e58f337074609d9faf678c
Kenneth Endfinger <kaendfinger@gmail.com>
2020-11-29 16:11:34 -0500

    virtual v0.0.2

--- Formula/virtual.rb
+++ Formula/virtual.rb
@@ -2,9 +2,9 @@ class Virtual < Formula
   desc "Linux Virtual Machines for macOS"
   homepage "https://github.com/kendfinger/virtual"
 
-  version "0.0.1"
-  url "https://github.com/kendfinger/virtual/releases/download/v0.0.1/virtual"
-  sha256 "a70cdc1111c47b54d9d47e012da83d144fe2f7144979c01eeefac7fdb46b6c06"
+  version "0.0.2"
+  url "https://github.com/kendfinger/virtual/releases/download/v0.0.2/virtual"
+  sha256 "559f78ae791af6ea2c6eae3ccdafffe221b5df0a582f2928b5119fa99d96d5a0"
 
   def install
     bin.install Dir["*"]
